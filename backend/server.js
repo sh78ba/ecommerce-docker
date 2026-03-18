1f73480c5fc0969281a61281de9b02ad092e81af
sh78ba <sh78ba@gmail.com>
2026-03-18 23:48:36 +0530

    server.js completed

--- backend/server.js
+++ backend/server.js
@@ -27,3 +27,36 @@ async function connectRabbitMQ() {
   channel = await conn.createChannel();
   await channel.assertQueue("orders");
 }
+connectRabbitMQ();
+
+//Get Products
+app.get("/products", async (req, res) => {
+  const cached = await reddisClient.get("products");
+
+  if (cached) {
+    return res.json(JSON.parse(cached));
+  }
+
+  const result = await pool.query("SELECT * from products");
+  await reddisClient.set("products", JSON.stringify(result.rows));
+  res.json(JSON.parse(result.rows));
+});
+
+// POST order
+
+app.post("/order", async (req, res) => {
+  const { product_id } = req.body;
+  const result = await pool.query(
+    "INSERT INTO orders(products_id, status) VALUES($1, $2) RETURNING *",
+    [product_id, "pending"],
+  );
+
+  //send to queue
+  channel.sendToQueue("orders", Buffer.from(JSON.stringify(result.rows[0])));
+
+  res.json({ message: "Order Placed Successfully", order: result.row[0] });
+});
+
+app.listen(5000, () => {
+  console.log("Backend Running on 5000");
+});
